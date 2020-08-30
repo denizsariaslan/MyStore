b2e568e88b0909d67a066eb4dd7814f917602d2b
Deniz Sariaslan <denizsariaslann@gmail.com>
2020-08-29 23:52:18 -0400

    1)I created a new folder as Models in MyStore.core. After I created Product.cs in this folder. 2)I added System.runtimecaching reference in references of  MyStore.DataAccess.InMemory and I choosed MyStore.core project. 3)I created ProductRepository.cs in MyStore.DataAccess.InMemory because I wanted to achieve some basic operation as CRUD or some query. 4)I created ProductManagerController in Controllers at MyStore.Web.UI

---
+++ MyStore.Core/MyStore.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyStore.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
